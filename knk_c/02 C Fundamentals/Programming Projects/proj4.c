71d03b3313d32298f86ec5b395061762bded4f9e
Alegruz <alegruz@khu.ac.kr>
2019-01-09 19:42:54 +0900

    Reconfiguring files

---
+++ knk_c/02 C Fundamentals/Programming Projects/proj4.c
@@ -0,0 +1,16 @@
+/* Dollars-and-cent amount to amount with 5% tax added */
+
+#include <stdio.h>
+
+#define TAX 0.05f
+
+int main(void) {
+  float amount;
+
+  printf("Enter an amount: ");
+  scanf("%f", &amount);
+  
+  printf("With tax added: $%.2f\n", amount * (1 + TAX));
+  
+  return 0;
+}
