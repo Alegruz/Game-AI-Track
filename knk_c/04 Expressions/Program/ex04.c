8aa53a7f456444f5fe8cb739244c25fa0b7bba9c
Alegruz <alegruz@khu.ac.kr>
2019-03-20 18:52:31 +0900

    Create ex04.c

---
+++ knk_c/04 Expressions/Program/ex04.c
@@ -0,0 +1,12 @@
+/* Computes four implementation-defined behavior expressions */
+/* Standard is C99 */
+
+#include <stdio.h>
+
+int main(void) {
+    printf("8 / 5: %d\n", 8 / 5); /* <1> */
+    printf("-8 / 5: %d\n", -8 / 5); /* <-1> */
+    printf("8 / -5: %d\n", 8 / -5); /* <-1> */
+    printf("-8 / -5: %d\n", -8 / -5); /* <1> */
+    return 0;
+}
