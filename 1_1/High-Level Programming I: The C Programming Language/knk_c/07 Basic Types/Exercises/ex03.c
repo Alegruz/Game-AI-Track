aad5bc202166203afb7bbe6f9a763211a2885241
Alegruz <alegruz@khu.ac.kr>
2019-04-10 19:25:12 +0900

    Create ex03.c

---
+++ 1_1/High-Level Programming I: The C Programming Language/knk_c/07 Basic Types/Exercises/ex03.c
@@ -0,0 +1,13 @@
+/* Shows the legal types in C */
+
+#include <stdio.h>
+
+int main(void)
+{
+    short unsigned int a;
+//    short float b;    INVALID
+    long double c;
+    unsigned long d;
+
+    return 0;
+}
