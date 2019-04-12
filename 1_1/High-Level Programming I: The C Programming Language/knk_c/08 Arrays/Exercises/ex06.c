a4ebc7b40d842faa8fec76d8247fa62afa9b8cb7
Alegruz <alegruz@khu.ac.kr>
2019-04-12 20:08:31 +0900

    removed #include <stdio.h>

--- 1_1/High-Level Programming I: The C Programming Language/knk_c/08 Arrays/Exercises/ex06.c
+++ 1_1/High-Level Programming I: The C Programming Language/knk_c/08 Arrays/Exercises/ex06.c
@@ -1,7 +1,5 @@
 /* Set up an array of seven-segment numbers */
 
-#include <stdio.h>
-
 int main (void) {
     const int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0},
                                  {0, 1, 1, 0, 0, 0, 0},
