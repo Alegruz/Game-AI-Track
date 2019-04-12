4408fa0dd25a31fd4890c641479856d2f66c24fa
Alegruz <alegruz@khu.ac.kr>
2019-04-12 20:00:43 +0900

    Create ex08.c

---
+++ 1_1/High-Level Programming I: The C Programming Language/knk_c/08 Arrays/Exercises/ex08.c
@@ -0,0 +1,12 @@
+/* Declare a two-dimensional array */
+
+#include <stdio.h>
+
+#define DAYS_IN_MONTH 30
+#define HOURS_IN_DAY 24
+
+int main(void) {
+    int temperature_readings[DAYS_IN_MONTH][HOURS_IN_DAY];
+
+    return 0;
+}
