126948475f19cc40699af8e70408c1ee0f091837
Alegruz <alegruz@khu.ac.kr>
2019-04-12 20:06:26 +0900

    Create ex09.c

---
+++ 1_1/High-Level Programming I: The C Programming Language/knk_c/08 Arrays/Exercises/ex09.c
@@ -0,0 +1,23 @@
+/* Computes average temperature for a month */
+
+#include <stdio.h>
+
+#define DAYS_IN_MONTH 30
+#define HOURS_IN_DAY 24
+
+int main(void) {
+    int temperature_readings[DAYS_IN_MONTH][HOURS_IN_DAY];
+    double average_daily_temperature[DAYS_IN_MONTH] = {0.00}, average_hourly_temperature[HOURS_IN_DAY] = {0.00};
+    double average_monthly_temperature = 0.00;
+
+    for (int day = 0; day < DAYS_IN_MONTH; ++day) {
+        for (int hour = 0; hour < HOURS_IN_DAY; ++hour) {
+            average_daily_temperature[day] += average_hourly_temperature[hour];
+        }
+        average_daily_temperature[day] /= HOURS_IN_DAY;
+        average_monthly_temperature += average_daily_temperature[day];
+    }
+    average_monthly_temperature /= DAYS_IN_MONTH;
+
+    return 0;
+}
