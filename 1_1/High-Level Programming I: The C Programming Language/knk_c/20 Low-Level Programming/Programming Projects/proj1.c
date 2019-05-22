332f19935f6018c84eb0229e7c6c231ceee92bf2
alegruz <alegruz@khu.ac.kr>
2019-05-22 22:35:40 +0900

    added programming projects of chapter 20

--- 1_1/High-Level Programming I: The C Programming Language/knk_c/20 Low-Level Programming/Programming Projects/proj1.c
+++ 1_1/High-Level Programming I: The C Programming Language/knk_c/20 Low-Level Programming/Programming Projects/proj1.c
@@ -1,2 +1,24 @@
 /* Design a union that makes it possible to view a 32-bit
 value as either a float or the structure described in ex14.c */
+
+#include <stdio.h>
+
+union IEEE_float {
+    float f;
+    struct {
+        unsigned int fraction: 23;
+        unsigned int exponent: 8;
+        unsigned int sign: 1;
+    } s_f;
+};
+
+int main(void) {
+    union IEEE_float u;
+    u.s_f.sign = 0;
+    u.s_f.exponent = 127;
+    u.s_f.fraction = 800000000;
+
+    printf("%.2g\n", u.f);
+
+    return 0;
+}
