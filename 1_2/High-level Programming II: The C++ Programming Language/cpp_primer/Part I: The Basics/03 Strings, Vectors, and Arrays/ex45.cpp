13578d2830621511427d1ee05f70c98dbb214818
alegruz <alegruz@khu.ac.kr>
2019-06-17 15:56:13 +0900

    added ex45.cpp of chapter 3.6

--- 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/03 Strings, Vectors, and Arrays/ex45.cpp
+++ 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/03 Strings, Vectors, and Arrays/ex45.cpp
@@ -1,29 +1,15 @@
 // Rewrite the programs again, this time using auto.
 #include <iostream>
-#include <cstddef>
-#include <iterator>
 
 int main() {
     int ia[2][3] = {0, 1, 2,
                     3, 4, 5};
 
-    // range for
-    for (auto &row : ia) {
-        for (auto &col : row)
-            std::cout << col << " ";
+    for (auto p = ia; p != ia + 2; ++p) {
+        for (auto q = *p; q != *p + 3; ++q)
+            std::cout << *q << " ";
         std::cout << std::endl;
     }
-    
-    // subscripts
-    for (size_t row = 0; row < 2; ++row) {
-        for (size_t col = 0; col < 3; ++col)
-            std::cout << ia[row][col] << " ";
-        std::cout << std::endl;
-    }
-
-    // pointers
-    for (int *p = &ia[0][0]; p <= &ia[1][2]; ++p)
-        std::cout << *p << " ";
 
     return 0;
 }
