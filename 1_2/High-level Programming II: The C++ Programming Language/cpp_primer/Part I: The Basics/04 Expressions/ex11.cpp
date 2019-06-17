e617d6318b862e60e4d0b4c28bf1709f707153b7
alegruz <alegruz@khu.ac.kr>
2019-06-17 18:54:19 +0900

    added ex11.cpp of chapter 4, section 4.3

---
+++ 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/04 Expressions/ex11.cpp
@@ -0,0 +1,11 @@
+// Write an expression that tests four values, a, b, c, and d,
+// and ensures that a is greater than b, which is greater than c, which is greater than d.
+
+int main() {
+    int a = 4, b = 2, c = 1, d = 0;
+
+    if (a > b && b > c && c > d)
+        ;
+
+    return 0;
+}
