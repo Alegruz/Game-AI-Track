2e672840e3260f9edce94005ac265b548824d999
alegruz <alegruz@khu.ac.kr>
2019-06-17 14:14:04 +0900

    added ex27.md and ex28.cpp of chapter 3

---
+++ 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/03 Strings, Vectors, and Arrays/ex28.cpp
@@ -0,0 +1,12 @@
+// What are the values in the following arrays?
+#include <string>
+
+using namespace std;
+
+string sa[10];  // {"", "", ... , ""}
+int ia[10]; // {0, 0, ... , 0}
+
+int main() {
+    string sa2[10]; // {"", "", ... , ""}
+    int    ia2[10]; // undefined
+}
