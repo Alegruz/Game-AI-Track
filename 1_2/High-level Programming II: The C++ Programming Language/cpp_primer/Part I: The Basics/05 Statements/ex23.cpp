6b133575b3f6773a1b1b449fab65b7d8e0d01a3f
alegruz <alegruz@khu.ac.kr>
2019-06-20 19:17:46 +0900

    int to double static cast and logic revision

--- 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/05 Statements/ex23.cpp
+++ 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/05 Statements/ex23.cpp
@@ -1,5 +1,5 @@
-// Write a program that reads two integers from the standard input
-// and prints the result of dividing the first number by the second.
+// Revise your program to throw an exception if the second number is zero.
+// Test your program with a zero input to see what happens on your system if you don’t catch an exception.
 #include <iostream>
 
 int main() {
@@ -10,7 +10,8 @@ int main() {
         try {
             if (divisor == 0)
                 throw std::out_of_range("0 cannot be a divisor.");
-            result = dividend / divisor;
+            result = static_cast<double>(dividend) / divisor;
+            std::cout << dividend << " / " << divisor << " is " << result << std::endl;
         } catch(const std::exception& e) {
             std::cerr << e.what() <<
                         "\nTry again? Enter y of n: " << std::endl;
@@ -19,8 +20,6 @@ int main() {
             if (!std::cin || c == 'n')
                 break;
         }
-
-    std::cout << dividend << " / " << divisor << " is " << result << std::endl;
 
     return 0;
 }
