d3dd27652461ff8b9d2c80a2dae8dde422971103
alegruz <alegruz@khu.ac.kr>
2019-06-20 18:58:28 +0900

    added ex21.cpp of chapter 5, section 5.5.2

--- 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/05 Statements/ex21.cpp
+++ 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/05 Statements/ex21.cpp
@@ -1,2 +1,28 @@
 // Revise the program from the exercise in § 5.5.1 (p. 191)
 // so that it looks only for duplicated words that start with an uppercase letter.
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string input_string;
+    std::string prev_input_string;
+
+    while (std::cin >> input_string)
+        if (prev_input_string.size() > 0 &&
+            'A' <= input_string[0] && input_string[0] <= 'Z' &&
+            input_string == prev_input_string)
+            break;
+        else {
+            prev_input_string = input_string;
+            input_string = EOF;
+        }
+
+
+    std::cout << std::endl;
+    if (prev_input_string == input_string)
+        std::cout << "The word is: " << prev_input_string << std::endl;
+    else
+        std::cout << "No repetition." << std::endl;
+
+    return 0;
+}
