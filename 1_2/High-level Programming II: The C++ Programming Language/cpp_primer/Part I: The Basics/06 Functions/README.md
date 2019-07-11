02f03c2d80bf4fe68e018074897d7c39b89bea12
alegruz <alegruz@khu.ac.kr>
2019-07-11 20:42:25 +0900

    added ex08 and ex09 of chapter 6, section 6.1.3

---
+++ 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/06 Functions/README.md
@@ -0,0 +1,2 @@
+## Best Practices
+The header that declares a function should be included in the source file that defines that function.
---
+++ 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/06 Functions/ex08/Chapter6.h
@@ -0,0 +1,13 @@
+// Write a header file named Chapter6.h that contains
+// declarations for the functions you wrote for the exercises in § 6.1 (p. 205).
+#if !defined(CHAPTER6_H)
+#define CHAPTER6_H
+#include <iostream>
+
+// Declaration
+int fact(int val);
+bool getFact();
+int getAbs(int val);
+
+#endif // CHAPTER6_H
+
