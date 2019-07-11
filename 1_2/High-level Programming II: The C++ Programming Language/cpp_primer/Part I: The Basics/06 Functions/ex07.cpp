45f4adeeb8072bac094197efeae0ed5155b7d71e
alegruz <alegruz@khu.ac.kr>
2019-07-11 20:26:45 +0900

    added ex06.md of chapter 6, section 6.1.1

---
+++ 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/06 Functions/ex07.cpp
@@ -0,0 +1,6 @@
+// Write a function that returns 0 when it is first called
+// and then generates numbers in sequence each time it is called again.
+int sequenceGenerator() {
+    static int callCount = 0;
+
+}
