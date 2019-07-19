1018710c5abf7098c974ab44a3d9e5edaca325fa
alegruz <alegruz@khu.ac.kr>
2019-07-19 19:40:35 +0900

    revised comments

--- 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/06 Functions/ex37.cpp
+++ 1_2/High-level Programming II: The C++ Programming Language/cpp_primer/Part I: The Basics/06 Functions/ex37.cpp
@@ -1,5 +1,6 @@
 // Write three additional declarations for the function in the previous exercise.
-// One should use a type alias, one should use a trailing return,
+// One should use a type alias,
+// one should use a trailing return,
 // and the third should use decltype.
 // Which form do you prefer and why?
 #include <string>
