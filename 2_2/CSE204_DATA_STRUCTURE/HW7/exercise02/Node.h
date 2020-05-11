061bcd734f5e3b0987f7708a51bedd732b69091e
alegruz <alegruz@khu.ac.kr>
2020-05-11 14:29:48 +0900

    removed prev pointer from node

--- 2_2/CSE204_DATA_STRUCTURE/HW7/exercise02/Node.h
+++ 2_2/CSE204_DATA_STRUCTURE/HW7/exercise02/Node.h
@@ -9,10 +9,8 @@ namespace hw7
 	{
 	public:
 		Node(std::unique_ptr<T> data);
-		Node(std::unique_ptr<T> data, std::shared_ptr<Node<T>> prev);
 		std::unique_ptr<T> Data;
 		std::shared_ptr<Node<T>> Next;
-		std::weak_ptr<Node<T>> Previous;
 	};
 
 	template<typename T>
@@ -20,14 +18,5 @@ namespace hw7
 		: Data(std::move(data))
 	{
 		Next = nullptr;
-		Previous.lock() = nullptr;
-	}
-
-	template<typename T>
-	Node<T>::Node(std::unique_ptr<T> data, std::shared_ptr<Node<T>> prev)
-		: Data(std::move(data))
-	{
-		Next = nullptr;
-		Previous = prev;
 	}
 }
