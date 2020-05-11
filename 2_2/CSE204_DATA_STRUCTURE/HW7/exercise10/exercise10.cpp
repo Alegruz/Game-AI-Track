bc9458312dba9bf197b0f83307e73c4affcad960
alegruz <alegruz@khu.ac.kr>
2020-05-11 15:43:06 +0900

    completed exercise10

---
+++ 2_2/CSE204_DATA_STRUCTURE/HW7/exercise10/exercise10.cpp
@@ -0,0 +1,24 @@
+/*
+
+a. The info member of Node 1, referenced from pointer C
+C->next->info
+
+b. The info member of Node 2, referenced from pointer B
+B->back->info
+
+c. The next member of Node 2, referenced from pointer A
+A->next->next
+
+d. The next member of Node 4, referenced from pointer C
+C->back->next
+
+e. Node 1, referenced from pointer B
+B->back->back
+
+f. The back member of Node 4, referenced from pointer C
+C->back->back
+
+g. The back member of Node 1, referenced from pointer A
+A->back
+ 
+ */
