0647e03a581b8bb9f1cdeac84c4969676dadbd31
alegruz <alegruz@khu.ac.kr>
2020-05-17 16:37:09 +0900

    completed exercise12

---
+++ 2_2/CSE204_DATA_STRUCTURE/HW8/exercise12.cpp
@@ -0,0 +1,11 @@
+/*
+ a. F. the function will always return 0. please use nullptr instead of NULL
+
+ b. D. the function doesn't produce sum of squared values but sum of their original values. please use nullptr instead of NULL
+
+ c. A-. works fine, but not using parenthese {} when you only have a single statement is a bad practice. please use nullptr instead of NULL
+
+ d. A0. works fine unless list is nullptr, but not using parenthese {} when you only have a single statement is a bad practice. please use nullptr instead of NULL
+
+ e. C-. wrong equation. will always return 0. please use nullptr instead of NULL
+ */
