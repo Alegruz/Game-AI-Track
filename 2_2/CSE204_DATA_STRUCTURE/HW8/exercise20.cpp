b1e9a1868f7355c7ffe6dd0808ed8e31bf09dd0c
alegruz <alegruz@khu.ac.kr>
2020-05-17 17:18:39 +0900

    completed exercise20

---
+++ 2_2/CSE204_DATA_STRUCTURE/HW8/exercise20.cpp
@@ -0,0 +1,19 @@
+/*
+ list
+ .length	20
+ .info		2	6	9	14	23	65	92	96	99	100
+			[0]	[1]	[2]	[3]	[4]	[5]	[6]	[7]	[8]	[9]
+
+ BinarySearch(info, 99, 0, 9);
+
+
+ call	fromLocation	toLocation	result	return address
+ 1		0				99			?>true	2048
+ 2		50				99			?>true	1024
+ 3		75				99			?>true	1024
+ 4		88				99			?>true	1024
+ 5		94				99			?>true	1024
+ 6		97				99			?>true	1024
+ 7		99				99			true	1024
+
+ */
