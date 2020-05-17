0d10ffdc22bd4842fcf0cf04c7012b3149b5d1b8
alegruz <alegruz@khu.ac.kr>
2020-05-17 16:37:30 +0900

    fixed grades

--- 2_2/CSE204_DATA_STRUCTURE/HW8/exercise12.cpp
+++ 2_2/CSE204_DATA_STRUCTURE/HW8/exercise12.cpp
@@ -3,9 +3,9 @@
 
  b. D. the function doesn't produce sum of squared values but sum of their original values. please use nullptr instead of NULL
 
- c. A-. works fine, but not using parenthese {} when you only have a single statement is a bad practice. please use nullptr instead of NULL
+ c. A0. works fine, but not using parenthese {} when you only have a single statement is a bad practice. please use nullptr instead of NULL
 
- d. A0. works fine unless list is nullptr, but not using parenthese {} when you only have a single statement is a bad practice. please use nullptr instead of NULL
+ d. B+. works fine unless list is nullptr, but not using parenthese {} when you only have a single statement is a bad practice. please use nullptr instead of NULL
 
  e. C-. wrong equation. will always return 0. please use nullptr instead of NULL
  */
