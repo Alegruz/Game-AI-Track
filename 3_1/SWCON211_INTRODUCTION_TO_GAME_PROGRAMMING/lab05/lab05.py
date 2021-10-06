69b59f7ae38cf5662270fdd091a8c6ab1a019a4d
Alegruz <alegruz@khu.ac.kr>
2021-10-06 20:22:57 +0900

    [ADD] added special features

--- 3_1/SWCON211_INTRODUCTION_TO_GAME_PROGRAMMING/lab05/lab05.py
+++ 3_1/SWCON211_INTRODUCTION_TO_GAME_PROGRAMMING/lab05/lab05.py
@@ -1,3 +1,4 @@
+from math import cos, radians, sin, sqrt
 import random
 
 from collections import defaultdict
@@ -38,30 +39,61 @@ class Actor(cocos.sprite.Sprite):
     def collide(self, other):
         pass
 
+class PlayerCannonEffect(Actor):
+    def __init__(self, x, y, opacity):
+        super(PlayerCannonEffect, self).__init__(image="img/cannon.png", x=x, y=y)
+        self.opacity = opacity
+        self.cshape = cm.AARectShape((-1, -1),
+                                     self.width * 0.5,
+                                     self.height * 0.5)
+    def update(self, elapsed):
+        self.opacity -= 255 * elapsed
+
+        if self.opacity <= 0:
+            self.kill()
+
 class PlayerCannon(Actor):
     KEYS_PRESSED = defaultdict(int)
+    SHOOT_RATE: float = 0.5
+    FLASH_RATE: float = 3.0
 
     def __init__(self, x, y):
         super(PlayerCannon, self).__init__('img/cannon.png', x, y)
         self.speed = eu.Vector2(200, 0)
         self.shoot_timer: float = 0.0
+        self.flash_timer: float = 0.0
+        self.shoot_mode: int = 1
+        self.current_score: int = None
 
     def update(self, elapsed):
+        if self.current_score is None:
+            self.current_score: int = self.parent.score
         self.shoot_timer = max(0.0, self.shoot_timer - elapsed)
+        self.flash_timer = max(0.0, self.flash_timer - elapsed)
         pressed = PlayerCannon.KEYS_PRESSED
         space_pressed = pressed[key.SPACE] == 1
-        # if PlayerShoot.INSTANCE is None and space_pressed:
         if space_pressed and self.shoot_timer == 0.0:
-            self.shoot_timer = 0.5
-            # self.parent.add(PlayerShoot(self.x, self.y + 50))
-            self.parent.shoot_laser(x=self.x, y=self.y + 50)
+            self.shoot_timer = self.SHOOT_RATE
+            self.parent.shoot_laser(x=self.x, y=self.y + 50, shoot_mode=self.shoot_mode)
 
         movement = pressed[key.RIGHT] - pressed[key.LEFT]
         w = self.width * 0.5
         if movement != 0 and w <= self.x <= self.parent.width - w:
-            self.move(self.speed * movement * elapsed, is_player=True)
+            if pressed[key.F] and self.flash_timer == 0.0:
+                self.flash_timer = self.FLASH_RATE
+                previous_position = self.position
+                self.move(self.speed * 40 * movement * elapsed, is_player=True)
+                flash_size: int = 4
+                for i in range(0, flash_size):
+                    self.parent.add(PlayerCannonEffect(x=((self.position[0] - previous_position[0]) * (i + 1) / flash_size) + previous_position[0],
+                                                   y=((self.position[1] - previous_position[1]) * (i + 1) / flash_size) + previous_position[1],
+                                                   opacity=(255 * (i + 1)) / flash_size))
+            else:
+                self.move(self.speed * movement * elapsed, is_player=True)
 
     def collide(self, other):
+        if isinstance(other, PlayerCannonEffect):
+            return
         other.kill()
         self.kill()
 
@@ -96,10 +128,37 @@ class GameLayer(cocos.layer.Layer):
         self.add(self.player)
         self.hud.update_lives(self.lives)
 
-    def shoot_laser(self, x: float, y: float):
-        laser: PlayerShoot = PlayerShoot(x=x, y=y)
-        self.add(laser)
-        self.laser_pool.append(laser)
+    def shoot_laser(self, x: float, y: float, shoot_mode: int = 1):
+        offset: float = self.player.width / (self.player.shoot_mode + 1)
+        for i in range(0, shoot_mode):
+            if shoot_mode % 2 == 1:
+                if i == shoot_mode // 2:
+                    laser: PlayerShoot = PlayerShoot(x=x - self.player.width / 2 + offset * (i + 1),
+                                                     y=y)
+                else:
+                    index: int = i
+                    if i > shoot_mode // 2:
+                        index = (shoot_mode // 2) - (i - shoot_mode // 2)
+                    angle: float = radians((((shoot_mode - 2 * index) * 180 / ((shoot_mode - 2 * index) + 2)) / 3) + 60)
+                    laser: PlayerShoot = PlayerShoot(x=x - self.player.width / 2 + offset * (i + 1),
+                                                     y=y,
+                                                     x_direction=(-1 if i < shoot_mode // 2 else 1) * cos(angle),
+                                                     y_direction=-sin(angle))
+            else:
+                if i == shoot_mode // 2 - 1 or i == shoot_mode // 2:
+                    laser: PlayerShoot = PlayerShoot(x=x - self.player.width / 2 + offset * (i + 1),
+                                                     y=y)
+                else:
+                    index: int = i
+                    if i > shoot_mode // 2:
+                        index = ((shoot_mode // 2) - 1) - (i - (shoot_mode // 2))
+                    angle: float = radians((((shoot_mode - 2 * index) * 180 / ((shoot_mode - 2 * index) + 2)) / 3) + 60)
+                    laser: PlayerShoot = PlayerShoot(x=x - self.player.width / 2 + offset * (i + 1),
+                                                     y=y,
+                                                     x_direction=(-1 if i < shoot_mode // 2 else 1) * cos(angle),
+                                                     y_direction=-sin(angle))
+            self.add(laser)
+            self.laser_pool.append(laser)
 
     def update_score(self, score=0):
         self.score += score
@@ -132,6 +191,18 @@ class GameLayer(cocos.layer.Layer):
         self.alien_group.update(dt)
         if random.random() < 0.001:
             self.add(MysteryShip(50, self.height - 50))
+
+        if self.score - self.player.current_score >= self.player.shoot_mode * 300:
+            self.player.shoot_mode += 1
+
+        is_alien_all_dead: bool = True
+        for column in self.alien_group.columns:
+            if len(column.aliens) != 0:
+                is_alien_all_dead = False
+                break
+
+        if is_alien_all_dead:
+            self.create_alien_group(100, 300)
 
 
     def collide(self, node):
@@ -232,21 +303,29 @@ class Shoot(Actor):
         self.speed = eu.Vector2(0, -400)
 
     def update(self, elapsed):
-        self.move(self.speed * elapsed)
+        self.move(offset=self.speed * elapsed)
 
 class PlayerShoot(Shoot):
     INSTANCE = None
 
-    def __init__(self, x, y, is_hidden: bool = True):
+    def __init__(self, x: float, y: float, x_direction: float = 0, y_direction: float = -1, is_hidden: bool = True):
         super(PlayerShoot, self).__init__(x, y, 'img/laser.png')
-        self.speed *= -1
+        # self.speed *= -1
+        self.velocity: float = self.speed[1]
+        self.speed[0] = x_direction / sqrt(x_direction ** 2 + y_direction ** 2) * self.velocity
+        self.speed[1] = y_direction / sqrt(x_direction ** 2 + y_direction ** 2) * self.velocity
+        print(f"speed: {self.speed}")
         # PlayerShoot.INSTANCE = self
 
     def collide(self, other):
         if isinstance(other, Alien):
             self.parent.update_score(other.score)
             self.parent.laser_pool.remove(self)
-            other.kill()
+            try:
+                other.kill()
+            except Exception as e:
+                print(f"Exception: {e}")
+
             self.kill()
 
     def on_exit(self):
